e4619977a85ee7ce432b39f9e91247ed983df31c
Deepjyoti Barman <deep.barman30@gmail.com>
2021-07-01 21:04:39 +0530

    Show the date on the post as well

--- components/PostEach.vue
+++ components/PostEach.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="post--each__container">
-    <div class="post--each--content py-6">
+    <div class="post--each--content py-6 border-b-2 flex">
+      <div class="left--content">
+        {{ getReadableDate(post.updated_at) }}
+      </div>
       <h1 class="text-4xl font-semibold">{{ post.title }}</h1>
     </div>
   </div>
@@ -14,6 +17,16 @@ export default {
       type: Object,
       require: true,
       default: null,
+    },
+  },
+  methods: {
+    getReadableDate(date) {
+      /**
+       * Parse the date into something more readable by the user.
+       */
+      return new Date(date).toLocaleDateString('en-US', {
+        dateStyle: 'short',
+      })
     },
   },
 }
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-50">
+  <div class="">
     <Navbar />
     <div
       class="
