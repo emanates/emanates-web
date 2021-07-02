ad9cd8e644949c84eadae1bcbcc9e6ddc4a7fe12
Deepjyoti Barman <deep.barman30@gmail.com>
2021-07-02 21:01:10 +0530

    Show the labels for blogs with the posts

--- components/PostEach.vue
+++ components/PostEach.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="post--each__container">
-    <div class="post--each--content py-6 border-b dark:border-gray-700 flex">
+    <div class="post--each--content py-8 border-b dark:border-gray-700 flex">
       <div class="left--content 2xl:w-1/5 xl:w-1/6 md:w-1/4 pt-1">
         <span
           class="
@@ -16,7 +16,7 @@
         <h1 class="text-3xl font-semibold dark:text-gray-200">
           {{ post.title }}
         </h1>
-        <p class="author--name mt-4">
+        <p class="author--name mt-2">
           <span class="text-gray-400 dark:text-gray-600">by</span>
           <a
             rel="noreferrer noopener"
@@ -26,6 +26,13 @@
             >{{ post.user.login }}</a
           >
         </p>
+        <div
+          v-if="post.labels.length"
+          class="label mt-3 py-1 px-2 inline-flex rounded-lg"
+          :class="getColorClassForLabel(post.labels[0].color)"
+        >
+          # {{ post.labels[0].name }}
+        </div>
       </div>
     </div>
   </div>
@@ -33,10 +40,11 @@
 
 <script>
 import { markdownRenderer } from '@/mixins/markdown'
+import { labels } from '@/mixins/labels'
 
 export default {
   name: 'PostEach',
-  mixins: [markdownRenderer],
+  mixins: [markdownRenderer, labels],
   props: {
     post: {
       type: Object,
@@ -52,6 +60,13 @@ export default {
       return new Date(date).toLocaleDateString('en-US', {
         dateStyle: 'medium',
       })
+    },
+    getColorClassForLabel(githubColorCode) {
+      /**
+       * Get the color code mapping for the label as
+       * returned by GitHub.
+       */
+      return this.getColor(githubColorCode)
     },
   },
 }
---
+++ mixins/labels.js
@@ -0,0 +1,19 @@
+export const labels = {
+    data() {
+        return {
+            colorMap: {
+                "0075ca": "bg-green-200",
+                "1089DD": "bg-blue-200"
+            }
+        }
+    },
+    methods: {
+        getColor(colorCode) {
+            /**
+             * Get the tailwind class for the color
+             * returned by GitHub.
+             */
+            return Object.keys(this.colorMap).includes(colorCode) ? this.colorMap[colorCode] : "bg-yellow-200";
+        }
+    }
+}
