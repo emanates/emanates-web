771bd3696b1efed67970f278908c9cf243783e11
Deepjyoti Barman <deep.barman30@gmail.com>
2021-07-03 15:41:49 +0530

    Use the nodeId to fetch details about the issue

--- pages/_post.vue
+++ pages/_post.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-  asyncData({ params }) {
+  async asyncData({ params }) {
     // Try to extract the issue nodeId
     const slug = params.post
     const nodeId = slug.split('-').slice(-1)[0]
@@ -13,6 +13,28 @@ export default {
     // raise error on that.
     /* eslint-disable no-console */
     if (!nodeId) return console.log('Error with slug')
+
+    const qlQuery = `query { node(id: "${nodeId}") {
+      ... on Issue {
+        title
+      }
+    } }`
+    const response = await fetch('https://api.github.com/graphql', {
+      method: 'POST',
+      headers: {
+        Authorization: `bearer ${process.env.EMANATES_TOKEN}`,
+        'Content-Type': 'appliction/json',
+      },
+      body: JSON.stringify({
+        query: qlQuery,
+      }),
+    })
+
+    // Check if error was thrown
+    console.log(response.status)
+
+    const json = await response.json()
+    return { post: json.data.node }
   },
 }
 </script>
