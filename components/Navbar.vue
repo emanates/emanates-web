7f1da040b295e6f732728a12d9deeb06c3a1881c
Deepjyoti Barman <deep.barman30@gmail.com>
2021-07-03 13:47:54 +0530

    Remove the powered by emantes from the branding

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,7 +4,7 @@
       class="
         bg-white
         dark:bg-gray-900
-        py-2
+        py-4
         w-full
         border-b border-gray-300
         dark:border-gray-700
@@ -12,14 +12,7 @@
     >
       <div class="navbar--content w-3/4 mr-auto ml-auto">
         <div class="brand">
-          <h1 class="font-bold text-lg dark:text-white">Blog</h1>
-          <a
-            href="https://github.com/emanates/emanates-web"
-            rel="noopener noreferer"
-            target="_blank"
-            class="text-sm text-gray-500"
-            >Powered by Emanates</a
-          >
+          <h1 class="font-bold text-lg dark:text-white">Emanates Blog</h1>
         </div>
       </div>
     </div>
