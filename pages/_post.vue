95c4b30723a6618d8932900a891bfaf58614905f
Deepjyoti Barman <deep.barman30@gmail.com>
2021-07-03 15:23:10 +0530

    Add init code to fetch issue using the slug

--- pages/_post.vue
+++ pages/_post.vue
@@ -5,7 +5,16 @@
 <script>
 export default {
   asyncData({ params }) {
-    console.log(params)
+    // Try to extract the issue ID
+    const slug = params.post
+    const id = slug.split('-').slice(-1)[0]
+
+    // The id would be undefined, if slug is invalid
+    // raise error on that.
+    /* eslint-disable no-console */
+    if (!id) return console.log('Error with slug')
+
+    // Now fetch the post with the id
   },
 }
 </script>
