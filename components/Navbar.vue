7cdab51c57ff7357aaab057f81857d94be328684
Deepjyoti Barman <deep.barman30@gmail.com>
2021-07-03 14:34:17 +0530

    Add the powered by emanates on the brand

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,7 +4,7 @@
       class="
         bg-white
         dark:bg-gray-900
-        py-4
+        py-2
         w-full
         border-b border-gray-300
         dark:border-gray-700
@@ -12,7 +12,14 @@
     >
       <div class="navbar--content w-3/4 mr-auto ml-auto">
         <div class="brand">
-          <h1 class="font-bold text-lg dark:text-white">Emanates Blog</h1>
+          <h1 class="font-bold text-lg dark:text-white">Blog</h1>
+          <a
+            href="https://github.com/emanates/emanates-web"
+            rel="noopener noreferer"
+            target="_blank"
+            class="text-sm text-gray-500"
+            >Powered by <span class="text-green-500">Emanates</span></a
+          >
         </div>
       </div>
     </div>
