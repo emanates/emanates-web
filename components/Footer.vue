3a455025cee2e12e7672d6f1177f1fe14d47dbcf
Deepjyoti Barman <deep.barman30@gmail.com>
2021-07-04 11:24:09 +0530

    Add colors for the footer on dark mode

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="footer__container border-t">
+  <div class="footer__container border-t dark:border-gray-700">
     <div class="footer--content w-3/4 mr-auto ml-auto py-6 flex text-gray-500">
       <div class="powered-by">
         Powered by
         <a
           href="https://emanat.es"
-          class="text-gray-700 font-semibold"
+          class="text-gray-700 dark:text-gray-300 font-semibold"
           target="_blank"
           rel="noopener noreferrer"
           >Emanates</a
         >
       </div>
-      <div class="separator border-l border-gray-700 mx-6"></div>
+      <div
+        class="separator border-l border-gray-700 dark:border-gray-500 mx-6"
+      ></div>
       <a
         href="https://github.com/emanates/emanates-web"
-        class="github flex items-center text-gray-700"
+        class="github flex items-center text-gray-700 dark:text-gray-300"
         target="_blank"
         rel="noopener noreferrer"
       >
