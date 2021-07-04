55b22c478e05a89c3d7527ab57c8a6e2cd64337c
Deepjyoti Barman <deep.barman30@gmail.com>
2021-07-04 11:15:18 +0530

    Add GitHub link to the Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,10 +3,29 @@
     <div class="footer--content w-3/4 mr-auto ml-auto py-6 flex text-gray-500">
       <div class="powered-by">
         Powered by
-        <a href="https://emanat.es" class="text-gray-700 underline">Emanates</a>
+        <a
+          href="https://emanat.es"
+          class="text-gray-700 font-semibold"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Emanates</a
+        >
       </div>
-      <div class="separator border-l border-gray-700 mx-4"></div>
-      <div class="github">We're Open Source</div>
+      <div class="separator border-l border-gray-700 mx-6"></div>
+      <a
+        href="https://github.com/emanates/emanates-web"
+        class="github flex items-center text-gray-700"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <p class="text">We're Open Source</p>
+        <img
+          src="@/assets/img/github-dark.png"
+          class="ml-3"
+          width="20"
+          alt="GitHub Logo"
+        />
+      </a>
     </div>
   </div>
 </template>
