5b47b2a5a5cd14ab7b4b68cd9ca5cb1395791bb7
Deepjyoti Barman <deep.barman30@gmail.com>
2021-07-09 19:01:47 +0530

    Add basic functionality to show the modal on share click

--- components/post/Share.vue
+++ components/post/Share.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="share__container">
     <div class="share--content font-normal">
-      <button type="button" title="Share" aria-label="Share" class="share--btn">
+      <button
+        type="button"
+        title="Share"
+        aria-label="Share"
+        class="share--btn"
+        @click="showShare"
+      >
         <client-only>
           <unicon name="upload-alt" fill="black" hover-fill="#34D399" />
         </client-only>
       </button>
+      <share-popup ref="sharePopup" />
     </div>
   </div>
 </template>
 
 <script>
+import SharePopup from '@/components/post/SharePopup.vue'
+
 export default {
   name: 'Share',
+  components: { SharePopup },
+  methods: {
+    showShare() {
+      /**
+       * Show the popup modal for the user to share the
+       * post.
+       */
+      this.$refs.sharePopup.$refs.popupModal.showModal()
+    },
+  },
 }
 </script>
---
+++ components/post/SharePopup.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="share--popup__container">
+    <Modal
+      ref="popupModal"
+      modal-id="share--modal"
+      heading="Share Post"
+      :cross-btn="true"
+    />
+  </div>
+</template>
+
+<script>
+import Modal from '../Modal.vue'
+export default {
+  components: { Modal },
+}
+</script>Modal
