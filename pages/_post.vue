c72cef093e8cb5eea8916f61dc1441a1392f79e2
Deepjyoti Barman <deep.barman30@gmail.com>
2021-07-24 13:08:23 +0530

    Make the post content width differ from the related

--- pages/_post.vue
+++ pages/_post.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="post 2xl:w-2/3 xl:w-3/4 lg:w-3/4 md:w-4/5 w-full mr-auto ml-auto">
-    <post-content :post="post" :related="related" />
+  <div class="post">
+    <div
+      class="
+        post--wrapper
+        2xl:w-2/3
+        xl:w-3/4
+        lg:w-3/4
+        md:w-4/5
+        w-full
+        mr-auto
+        ml-auto
+      "
+    >
+      <post-content :post="post" :related="related" />
+    </div>
   </div>
 </template>
 
