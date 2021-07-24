14e49dc8d72863f6f728a58c43975da587b1af69
Deepjyoti Barman <deep.barman30@gmail.com>
2021-07-24 12:49:43 +0530

    Make the related post look better

--- components/post/RelatedPost.vue
+++ components/post/RelatedPost.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="related--post__container">
-    <h1 class="text-xl font-medium">Easily Test Mixins with Jest</h1>
+    <div class="related--post--content flex py-4">
+      <div class="author mr-4">
+        <img
+          src="https://avatars.githubusercontent.com/u/33604454?u=561b31772e4ca95112723f289061918938112393&v=4"
+          width="40"
+          alt="Author Avatar"
+          class="rounded-full"
+        />
+      </div>
+      <div class="post--details">
+        <h1 class="text-2xl font-semibold">Easily Test Mixins with Jest</h1>
+        <p class="text-gray-600 font-medium mt-2">Jul 16, 2021</p>
+      </div>
+    </div>
   </div>
 </template>
 
