bd2a97cd07d19f6ff333a1365374b8b0d8b0b6f6
Deepjyoti Barman <deep.barman30@gmail.com>
2021-07-24 12:22:36 +0530

    Add the related posts container on the post page

--- components/PostContent.vue
+++ components/PostContent.vue
@@ -9,16 +9,18 @@
       :post="post"
       class="2xl:w-5/6 xl:w-4/5 w-11/12 mr-auto ml-auto"
     />
+    <related-posts :post="post" />
   </div>
 </template>
 
 <script>
 import Cover from './post/Cover.vue'
 import PostText from './post/PostText.vue'
+import RelatedPosts from './post/RelatedPosts.vue'
 const { default: PostHeader } = require('./post/PostHeader.vue')
 
 export default {
-  components: { PostHeader, PostText, Cover },
+  components: { PostHeader, PostText, Cover, RelatedPosts },
   props: {
     post: {
       type: Object,
