141a05529b82085fc63b1528a82c930e542c5305
Deepjyoti Barman <deep.barman30@gmail.com>
2021-07-24 11:40:34 +0530

    Add a plugin to inject the getPosts and get related

---
+++ components/post/RelatedPosts.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="related--posts__container"></div>
+</template>
+
+<script>
+export default {
+  name: 'RelatedPosts',
+  props: {
+    post: {
+      type: Object,
+      default: null,
+      require: true,
+    },
+  },
+}
+</script>
