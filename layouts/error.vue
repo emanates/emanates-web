aa1c1b5282a0b441b9056ff822127ac290412f47
Deepjyoti Barman <deep.barman30@gmail.com>
2021-08-01 21:13:49 +0530

    Design the 404 page with some nice text

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,8 +1,37 @@
 <template>
-  <div>
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <NuxtLink to="/">Home page</NuxtLink>
+  <div
+    class="
+      not-found--container
+      2xl:w-2/3
+      xl:w-3/4
+      lg:w-3/4
+      md:w-4/5
+      w-11/12
+      mr-auto
+      ml-auto
+      flex
+      items-center
+    "
+  >
+    <div class="wrapper block">
+      <div class="top--generic--content">
+        <h1 class="text-4xl font-bold">
+          Uh Oh! Seems like you're at the wrong place.
+        </h1>
+        <p class="mt-3 text-lg font-medium text-gray-800">
+          We tried our best but couldn't find the page you're looking for!
+        </p>
+      </div>
+      <div v-if="error" class="error--details mt-12 flex text-lg">
+        <p>{{ error.statusCode }}</p>
+        <span class="border-l-2 border-gray-500 mx-4"></span>
+        <p>{{ error.message }}</p>
+      </div>
+      <div class="extra--message mt-8 text-gray-700">
+        > If you think this is an error on our side,
+        <a href="">you can raise an issue on Emanates</a>!
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,3 +45,9 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.not-found--container {
+  height: 80vh;
+}
+</style>
