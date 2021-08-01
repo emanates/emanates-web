5f0183bdb454a7eb0c3d232922badf6fcb2a45a3
Deepjyoti Barman <deep.barman30@gmail.com>
2021-08-01 21:18:02 +0530

    Adapt the error page for dark mode

--- layouts/error.vue
+++ layouts/error.vue
@@ -14,26 +14,37 @@
     "
   >
     <div class="wrapper block">
-      <div class="top--generic--content">
+      <div class="top--generic--content dark:text-white">
         <h1 class="text-4xl font-bold">
           Uh Oh! Seems like you're at the wrong place.
         </h1>
-        <p class="mt-3 text-lg font-medium text-gray-700">
+        <p class="mt-3 text-lg font-medium text-gray-700 dark:text-gray-400">
           We tried our best but couldn't find the page you're looking for!
         </p>
       </div>
-      <div v-if="error" class="error--details mt-12 flex text-lg font-semibold">
+      <div
+        v-if="error"
+        class="
+          error--details
+          mt-12
+          flex
+          text-lg
+          font-semibold
+          text-gray-800
+          dark:text-gray-300
+        "
+      >
         <p>{{ error.statusCode }}</p>
         <span class="border-l-2 border-gray-500 mx-3"></span>
         <p>{{ error.message }}</p>
       </div>
-      <div class="extra--message mt-8 text-gray-700">
+      <div class="extra--message mt-8 text-gray-700 dark:text-gray-400">
         > If you think this is an error on our side,
         <a
           href="https://github.com/emanates/emanates-web/issues/new"
           target="_blank"
           rel="noreferrer noopener"
-          class="text-gray-900 font-medium underline"
+          class="text-gray-900 dark:text-gray-300 font-medium underline"
           >you can raise an issue on Emanates</a
         >!
       </div>
