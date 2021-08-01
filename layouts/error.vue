0ef6eef9fddcef0c50d4303dc85109fbf80b0b3f
Deepjyoti Barman <deep.barman30@gmail.com>
2021-08-01 20:28:54 +0530

    Override the error page in the layouts

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
--- pages/not-found.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="not--found__container">This is a 404 error page</div>
-</template>
-
-<script>
-export default {
-  name: 'NotFound',
-}
-</script>
