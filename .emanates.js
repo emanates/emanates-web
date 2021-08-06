97cc7ef1645fbfc4b90aa0bdb7af14707144626f
Deepjyoti Barman <deep.barman30@gmail.com>
2021-08-06 18:58:22 +0530

    Put an example config file

---
+++ .emanates.js
@@ -0,0 +1,11 @@
+module.exports = {
+    seo: {
+        site_name: "Blog | Built with Emanates",
+        site_description: "Just another blog built with Emanates",
+        site_keywords: "blog,emanates,built,emanates",
+        twitter_username: "@Emanates",
+        base_url: "https://emanates.deepjyoti30.dev",
+        logo: "https://deepjyoti30.dev/img/icons/favicon-32x32.png"
+    },
+    favicon_url: "https://deepjyoti30.dev/img/icons/favicon-16x16.png"
+}
