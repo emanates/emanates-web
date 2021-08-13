b5e073be99a37b082000c0b00b1d3ac67db47f68
Deepjyoti Barman <deep.barman30@gmail.com>
2021-08-13 20:56:59 +0530

    Add init content for the comments

---
+++ components/post/Comments.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="comments__container mt-18 md:mb-18 mb-14">
+    <h4
+      class="
+        text-2xl text-gray-700
+        dark:text-gray-400
+        md:ml-0
+        ml-2
+        font-semibold
+        text-center
+      "
+    >
+      Comments
+    </h4>
+    <div class="comments--content mt-8">
+      <script
+        src="https://utteranc.es/client.js"
+        repo="deepjyoti30/blog-comments"
+        issue-term="pathname"
+        theme="github-light"
+        crossorigin="anonymous"
+        async
+      ></script>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
